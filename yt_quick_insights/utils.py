f2fdc2917a2e7d9b21befa070f0774ff032049fd
Tendai <tendai.charura@gmail.com>
2024-08-14 21:49:36 +0700

    Add better exception handling for loading yml file

--- yt_quick_insights/utils.py
+++ yt_quick_insights/utils.py
@@ -1,11 +1,8 @@
 import re
-from enum import Enum
-from pathlib import Path
 
 import yaml
 
-PROJECT_DIR = Path(__file__).resolve().parent
-HOME_DIR = Path.home()
+from yt_quick_insights.config import settings
 
 
 def clean_youtube_title(video_title: str) -> str:
@@ -22,52 +19,28 @@ def clean_youtube_title(video_title: str) -> str:
     return re.sub(r"\s+", " ", allowed_chars).replace(" ", "_")[0:200]
 
 
-def load_yaml_file(file_name: str, user_yaml: bool = False) -> dict:
+def load_yaml_file(file_name: str, user_yaml: bool = False) -> dict[str, str]:
     """
-    Load a YAML file.
+    Load a YAML file from either the project directory or the user's home directory
 
     Args:
         file_name: The name of the YAML file
-        user_yaml: Whether to load the YAML file from the user's home directory
+        user_yaml: If True, load from user's home directory; otherwise, from project directory.
 
     Returns:
         The content of the YAML file as a dictionary
+
+    Raises:
+        yaml.YAMLError: If there is an error parsing the YAML file
     """
-    yaml_directory = HOME_DIR / ".insights" if user_yaml else PROJECT_DIR / "data"
+    yaml_directory = (
+        settings.HOME_DIR / ".insights" if user_yaml else settings.PROJECT_DIR / "data"
+    )
     yaml_file = yaml_directory / file_name
 
-    with open(yaml_file, "r") as f:
-        yaml_content = yaml.safe_load(f)
-
-    return yaml_content
-
-
-def combine_default_and_user_task_details(file_name: str = "task_details.yml") -> dict:
-    """
-    Combine default task details with user task details, both configured in YAML files.
-    In case of duplicates, user task details will overwrite default task details.
-
-    Args:
-        file_name: The name of the YAML file, default is "task_details.yml"
-
-    Returns:
-        The combined task details as a dictionary
-    """
-    default_task_details = load_yaml_file(file_name)
     try:
-        user_task_details = load_yaml_file(file_name, user_yaml=True)
-        combined_dicts = {**default_task_details, **user_task_details}
-    except FileNotFoundError:
-        combined_dicts = default_task_details
-
-    return dict(sorted(combined_dicts.items()))
-
-
-tasks = combine_default_and_user_task_details()
-
-
-def create_task_details_enum():
-    return Enum("TaskDetails", {key: key for key in tasks.keys()}, type=str)
-
-
-TaskDetails = create_task_details_enum()
+        with open(yaml_file, "r") as file:
+            yaml_content = yaml.safe_load(file)
+        return yaml_content
+    except yaml.YAMLError as e:
+        raise yaml.YAMLError(f"Error parsing YAML file {yaml_file}: {e}")
