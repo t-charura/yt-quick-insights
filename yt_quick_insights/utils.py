eb189c1f010864a2fee5f97b25d053ba11b0f157
Tendai <tendai.charura@gmail.com>
2024-08-14 07:56:24 +0700

    Add function to combine default and user yaml files

--- yt_quick_insights/utils.py
+++ yt_quick_insights/utils.py
@@ -1,27 +1,10 @@
-import os
 import re
 from pathlib import Path
 
 import yaml
 
 PROJECT_DIR = Path(__file__).resolve().parent
-
-
-def load_yaml_file(file_name: str) -> dict:
-    """
-    Load a YAML file.
-
-    Args:
-        file_name: The name of the YAML file
-
-    Returns:
-        The content of the YAML file as a dictionary
-    """
-    yml_file = os.path.join(PROJECT_DIR / "data" / file_name)
-    with open(yml_file, "r") as f:
-        yml_content = yaml.safe_load(f)
-
-    return yml_content
+HOME_DIR = Path.home()
 
 
 def clean_youtube_title(video_title: str) -> str:
@@ -36,3 +19,45 @@ def clean_youtube_title(video_title: str) -> str:
     """
     allowed_chars = re.sub(r"[^a-z0-9 ]", "", video_title.lower()).strip()
     return re.sub(r"\s+", " ", allowed_chars).replace(" ", "_")[0:200]
+
+
+def load_yaml_file(file_name: str, user_yaml: bool = False) -> dict:
+    """
+    Load a YAML file.
+
+    Args:
+        file_name: The name of the YAML file
+        user_yaml: Whether to load the YAML file from the user's home directory
+
+    Returns:
+        The content of the YAML file as a dictionary
+    """
+    yaml_directory = HOME_DIR / ".insights" if user_yaml else PROJECT_DIR / "data"
+    yaml_file = yaml_directory / file_name
+
+    with open(yaml_file, "r") as f:
+        yaml_content = yaml.safe_load(f)
+
+    return yaml_content
+
+
+def combine_default_and_user_task_details(file_name: str = "task_details.yml") -> dict:
+    """
+    Combine default task details with user task details, both configured in YAML files.
+    In case of duplicates, user task details will overwrite default task details.
+
+    Args:
+        file_name: The name of the YAML file, default is "task_details.yml"
+
+    Returns:
+        The combined task details as a dictionary
+    """
+    default_task_details = load_yaml_file(file_name)
+    try:
+        user_task_details = load_yaml_file(file_name, user_yaml=True)
+        return {**default_task_details, **user_task_details}
+    except FileNotFoundError:
+        return default_task_details
+
+
+task_details = combine_default_and_user_task_details()
