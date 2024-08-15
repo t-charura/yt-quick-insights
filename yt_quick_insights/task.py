d2340de4322217a861a363acd94e943a8bdc2350
Tendai <tendai.charura@gmail.com>
2024-08-15 10:56:44 +0700

    Add docstrings to TaskManager class

--- yt_quick_insights/task.py
+++ yt_quick_insights/task.py
@@ -5,23 +5,52 @@ from yt_quick_insights import utils
 
 
 class TaskManager:
+    """
+    This class provides functionality to load and merge the default and user-defined tasks.
+    Additionally, it creates an Enum object for all tasks.
+    """
 
     def __init__(self, file_name: str = "task_details.yml"):
+        """
+        Initialize the TaskManager class
+
+        Args:
+            file_name: The name of the YAML file containing the default and user tasks.
+        """
         self.file_name = file_name
         self.default_tasks = utils.load_yaml_file(self.file_name)
         self.user_tasks = self._load_user_tasks()
         self.tasks = self._merge_tasks()
 
     def _load_user_tasks(self) -> dict[str, str]:
+        """
+        Load user-defined tasks from a YAML file.
+
+        Returns:
+            A dictionary of user-defined tasks.
+        """
         try:
             return utils.load_yaml_file(self.file_name, user_yaml=True)
         except FileNotFoundError:
             return {}
 
     def _merge_tasks(self) -> dict[str, str]:
+        """
+        Merge the default tasks with the user-defined tasks.
+        User-defined tasks override the default tasks.
+
+        Returns:
+            A merged and sorted dictionary of tasks.
+        """
         return dict(sorted({**self.default_tasks, **self.user_tasks}.items()))
 
     def create_task_enum(self) -> Type[Enum]:
+        """
+        Create an Enum object with the keys of the tasks dictionary.
+
+        Returns:
+            An Enum object with the keys of the tasks dictionary.
+        """
         return Enum("TaskDetails", {key: key for key in self.tasks.keys()})
 
 
