18c7df7e7d47fe607b73cd5cc33f6cee3c092b0a
Tendai <tendai.charura@gmail.com>
2024-08-28 14:01:23 +0700

    Add several helper functions

--- yt_quick_insights/utils.py
+++ yt_quick_insights/utils.py
@@ -2,6 +2,9 @@ import re
 from pathlib import Path
 
 import yaml
+from langchain_openai import ChatOpenAI
+from rich.console import Console
+from rich.markdown import Markdown
 
 from yt_quick_insights.config import settings
 
@@ -44,6 +47,32 @@ def load_yaml_file(file_name: str, directory: Path) -> dict[str, str]:
         raise yaml.YAMLError(f"Error parsing YAML file {yaml_file}: {e}")
 
 
+def initialize_llm(model_name: str, api_key: str) -> ChatOpenAI:
+    """
+    Return an LLM instance
+
+    Returns:
+        LLM instance
+    """
+    return ChatOpenAI(
+        model_name=model_name,
+        api_key=api_key,
+        temperature=0,
+    )
+
+
+def save_to_file(file_name: str, content: str) -> None:
+    """
+    Save the given result to a file with the given file name.
+
+    Args:
+        file_name: The name of the file to save to.
+        content: The content to save to the file.
+    """
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(content)
+
+
 def get_yaml_location() -> str:
     """
     Get the location of the yaml file where the user can define their task details.
@@ -56,3 +85,20 @@ def get_yaml_location() -> str:
         f'--> [green bold]{settings.HOME_DIR / ".insights" / "task_details.yaml"}[/green bold] <--\n\n'
         f"Check https://github.com/t-charura/yt-quick-insights for an example yaml file."
     )
+
+
+def env_information(file_path: Path) -> str:
+    return (
+        "Language Transfer Flashcards (ltf) is looking for the .env file at the following location:\n"
+        f"--> [green bold]{file_path}[/green bold] <--\n\n"
+        "File must contain the following variables: 'OPENAI_API_KEY', 'OPENAI_MODEL_NAME' and 'TARGET_LANGUAGE'. \n"
+        "To see all valid values for these variables, run 'ltf csv --help'\n\n"
+        # TODO: update link for example .env file (add a screenshot to README)
+        "Check https://github.com/t-charura/yt-quick-insights for an example .env file."
+    )
+
+
+def show_markdown_output(string) -> None:
+    """Show markdown output in the console."""
+    console = Console()
+    console.print(Markdown(string))
