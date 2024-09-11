d0d8b3772433df4e8099f12abe0987e414d89434
Tendai <tendai.charura@gmail.com>
2024-09-11 09:21:18 +0700

    Add playlist_insights page, page_config and adjust navigation

--- yt_quick_insights/frontend/streamlit_app.py
+++ yt_quick_insights/frontend/streamlit_app.py
@@ -3,11 +3,23 @@ import streamlit as st
 from yt_quick_insights.config import settings
 
 # Page Setup
-homepage = st.Page(
-    page="views/homepage.py",
-    title="YouTube Quick Insights",
+video_insights = st.Page(
+    page="views/video_insights.py",
+    title="From Video",
     icon=":material/youtube_activity:",
     default=True,
+)
+
+playlist_insights = st.Page(
+    page="views/playlist_insights.py",
+    title="From Playlist",
+    icon=":material/playlist_play:",
+)
+
+extraction_methods = st.Page(
+    page="views/extraction_methods.py",
+    title="Extraction Methods",
+    icon=":material/search_insights:",
 )
 
 custom_extraction_method = st.Page(
@@ -22,32 +34,25 @@ default_values_with_env = st.Page(
     icon=":material/text_snippet:",
 )
 
-task_details = st.Page(
-    page="views/task_details.py",
-    title="Available Extraction Methods",
-    icon=":material/search_insights:",
-)
 
-
-contact = st.Page(
-    page="views/contact.py",
-    title="Contact",
-    icon=":material/alternate_email:",
+st.set_page_config(
+    layout="wide",
+    menu_items={"About": "https://github.com/t-charura/yt-quick-insights"},
 )
 
 # Navigation
 pg = st.navigation(
     {
-        "Insights": [homepage, task_details],
+        "Extract Insights": [video_insights, playlist_insights],
+        "Help": [extraction_methods],
         "Configuration": [custom_extraction_method, default_values_with_env],
-        "Contact": [contact],
     }
 )
 
 st.logo(str(settings.PROJECT_DIR / ".." / "docs" / "images" / "logo.png"))
 
 st.sidebar.markdown(
-    "Made with :material/favorite: by [Tendai](https://github.com/t-charura)"
+    ":material/build_circle: Created by [Tendai](https://github.com/t-charura)"
 )
 
 
