08faffa6f855103f97e9fe9e97fadaabff339222
Tendai <tendai.charura@gmail.com>
2024-09-13 15:15:22 +0700

    Add borders for better layout by using st.container

--- yt_quick_insights/frontend/views/playlist_insights.py
+++ yt_quick_insights/frontend/views/playlist_insights.py
@@ -10,8 +10,8 @@ from yt_quick_insights.frontend import components
 
 def initialize_session_state():
     """Initialize session states variables."""
-    if "submitted" not in st.session_state:
-        st.session_state.submitted = False
+    if "submitted_playlist" not in st.session_state:
+        st.session_state.submitted_playlist = False
     if "playlist_insights" not in st.session_state:
         st.session_state.playlist_insights = ""
 
@@ -29,15 +29,17 @@ def display_help():
             """
             1. **Playlist URL**: Paste the YouTube playlist URL in the designated field (required).
             2. **Additional Instructions**: Additional instructions for summarizing the playlist (optional).
+                - Example: Playlist with 10 videos about "Productivity"
+                - Additional Instructions: "What is the best way to do Goal Setting?"
             3. **Extraction Method**: Select an appropriate method based on the video content. When in doubt, 
                use the `General Summary` method. 
                For detailed information on available methods, click [here](/extraction_methods).
-            4. **OpenAI API Key**: Provide your API key in one of the following ways:
+            4. **OpenAI Model**: Choose your preferred model (default: `gpt-4o-mini`).
+               View all available models [here](https://platform.openai.com/docs/models).
+            5. **OpenAI API Key**: Provide your API key in one of the following ways:
                - Enter it directly in the provided field.
                - Set it in the `.env` file (recommended). Learn more [here](/env_file).
                - Store the key in an environment variable called: `OPENAI_API_KEY`.
-            5. **OpenAI Model**: Choose your preferred model (default: `gpt-4o-mini`).
-               View all available models [here](https://platform.openai.com/docs/models).
             """,
             unsafe_allow_html=True,
         )
@@ -62,6 +64,11 @@ def process_user_inputs():
         if not model_name:
             model_name = settings.OPENAI_MODEL_NAME
 
+        if not additional_instructions:
+            additional_instructions = (
+                "No additional instructions, focus on the instructions above"
+            )
+
         try:
             st.session_state.playlist_insights = caching.extract_playlist_insights(
                 playlist_url=playlist_url,
@@ -70,23 +77,23 @@ def process_user_inputs():
                 model_name=model_name,
                 api_key=api_key,
             )
-            st.session_state.submitted = True
+            st.session_state.submitted_playlist = True
         except (HTTPError, KeyError):
             st.error(
                 "Please provide a valid YouTube Playlist URL in the form of 'https://www.youtube.com/playlist?list=PLAYLIST_ID'"
             )
-            st.session_state.submitted = False
+            st.session_state.submitted_playlist = False
         except typer.Abort as e:
             components.display_openai_errors(e, model_name)
-            st.session_state.submitted = False
+            st.session_state.submitted_playlist = False
 
 
 def display_results():
-    if st.session_state.submitted:
-        st.markdown(st.session_state.playlist_insights)
+    if st.session_state.submitted_playlist:
+        result_container = st.container(border=True)
+        result_container.markdown(st.session_state.playlist_insights)
 
         if st.session_state.playlist_insights:
-            st.divider()
             col1, col2 = st.columns(2)
 
             file_format = col2.radio("File format", (".txt", ".md"))
