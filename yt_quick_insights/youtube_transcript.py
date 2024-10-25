95d0e6df0c3ef7edaa22c65d9b3321221c2a9dba
Tendai <tendai.charura@gmail.com>
2024-10-25 19:06:03 +0700

    Fix common pytube bug with workaround (account for changes in YouTube API structure)

--- yt_quick_insights/youtube_transcript.py
+++ yt_quick_insights/youtube_transcript.py
@@ -2,6 +2,8 @@ import re
 from typing import Tuple
 
 from langchain_community.document_loaders import YoutubeLoader
+from langchain_core.documents import Document
+from pytube.exceptions import PytubeError
 
 
 class YoutubeTranscript:
@@ -29,12 +31,23 @@ class YoutubeTranscript:
         """
         try:
             # Try loading the transcript from the YouTube URL - not all videos have transcripts
-            loader = YoutubeLoader.from_youtube_url(
-                youtube_url=video_url,
-                language=video_language,
+            yt_document = self._load(
+                video_url=video_url,
+                video_language=video_language,
                 add_video_info=True,
             )
-            yt_document = loader.load()[0]
+        except PytubeError:
+            # Common error, when YouTube makes changes to their API structure and "pytube" has not yet adjusted
+            # pytube is used by langchain's YoutubeLoader to fetch video details
+            yt_document = self._load(
+                video_url=video_url,
+                video_language=video_language,
+                add_video_info=False,
+            )
+            return (
+                "YouTube Video Title could not be retrieved",
+                self._clean_text(yt_document.page_content),
+            )
         except ValueError:
             raise ValueError(
                 "Please provide a valid YouTube URL "
@@ -49,6 +62,29 @@ class YoutubeTranscript:
             self._clean_text(yt_document.metadata.get("title")),
             self._clean_text(yt_document.page_content),
         )
+
+    @staticmethod
+    def _load(
+        video_url: str, video_language: str | list[str], add_video_info: bool
+    ) -> Document:
+        """
+        Load the transcript from the YouTube URL with the option to add video info.
+
+        Args:
+            video_url: The URL of the YouTube video. Example: "https://www.youtube.com/watch?v=VIDEO_ID"
+            video_language: The language of the transcript. Example: "en" or ["en", "de"]
+            add_video_info: Whether to add video info to the transcript. Example: True or False
+
+        Returns:
+            A Document object containing the transcript of the YouTube video and metadata.
+
+        """
+        loader = YoutubeLoader.from_youtube_url(
+            youtube_url=video_url,
+            language=video_language,
+            add_video_info=add_video_info,
+        )
+        return loader.load()[0]
 
     @staticmethod
     def _clean_text(text: str) -> str:
